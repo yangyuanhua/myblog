2a654b58ad42839b005b7926651cb006b9d8e012
yangyuanhua <123@126.com>
2020-05-31 15:24:24 +0800

    auto deploy

---
+++ _credentials.py
@@ -0,0 +1,2 @@
+github_username = 'yangyuanhua'
+github_password = 'YYh13797493269'
--- myblog/settings/local.py
+++ myblog/settings/local.py
@@ -5,4 +5,4 @@ from .common import *
 SECRET_KEY = '=hm@ily9+g#1@0f&u#9(gpb216l#09)05f9h22y(d7*h9#i5--'
 
 DEBUG = True
-ALLOWED_HOSTS = ['*',".101.133.210.166"]
+ALLOWED_HOSTS = ['*']
