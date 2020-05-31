59d4a404178078e0c17c87997a22c2aaa183cfeb
yangyuanhua <yangyuanhua_hata@126.com>
2020-05-31 19:04:19 +0800

    auto deploy10

--- fabfile.py
+++ fabfile.py
@@ -43,7 +43,7 @@ def deploy(c):
     with c.cd(project_root_path):
         c.run('pipenv install --deploy --ignore-pipfile')
         c.run('pipenv run python manage.py makemigrations')
-	c.run('pipenv run python manage.py migrate')
+        c.run('pipenv run python manage.py migrate')
         c.run('pipenv run python collectstatic --noinput')
         print("部署")
 
